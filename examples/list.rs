8714d888ddacfaf52ece7e1898e31a33bf04c5b2
meh <meh@schizofreni.co>
2017-03-02 22:06:49 +0100

    examples/list: make it look a little more like lsusb

--- examples/list.rs
+++ examples/list.rs
@@ -4,12 +4,21 @@ fn main() {
 	let hid = hid::init().unwrap();
 
 	for device in hid.devices() {
-		println!(
-			"0x{:x}:0x{:x} - {:?}: {:?}",
-			device.vendor_id(),
-			device.product_id(),
-			device.manufacturer_string(),
-			device.product_string()
-		);
+		print!("{} ", device.path().to_str().unwrap());
+		print!("ID {:x}:{:x} ", device.vendor_id(), device.product_id());
+
+		if let Some(name) = device.manufacturer_string() {
+			print!("{} ", name);
+		}
+
+		if let Some(name) = device.product_string() {
+			print!("{} ", name);
+		}
+
+		if let Some(name) = device.serial_number() {
+			print!("{} ", name);
+		}
+
+		println!();
 	}
 }
