fef431ad87c6806e9de43517f359a17ab6ccc16f
Nate Mara <natemara@gmail.com>
2020-07-01 11:59:41 -0700

    Run cargo fmt across the project
    This resolves @meh's comment on #8

--- src/devices.rs
+++ src/devices.rs
@@ -5,47 +5,47 @@ use Device;
 
 /// An iterator over the available devices.
 pub struct Devices<'a> {
-	ptr: *mut hid_device_info,
-	cur: *mut hid_device_info,
+    ptr: *mut hid_device_info,
+    cur: *mut hid_device_info,
 
-	_marker: PhantomData<&'a ()>,
+    _marker: PhantomData<&'a ()>,
 }
 
 impl<'a> Devices<'a> {
-	#[doc(hidden)]
-	pub unsafe fn new(vendor: Option<u16>, product: Option<u16>) -> Self {
-		let list = hid_enumerate(vendor.unwrap_or(0), product.unwrap_or(0));
+    #[doc(hidden)]
+    pub unsafe fn new(vendor: Option<u16>, product: Option<u16>) -> Self {
+        let list = hid_enumerate(vendor.unwrap_or(0), product.unwrap_or(0));
 
-		Devices {
-			ptr: list,
-			cur: list,
+        Devices {
+            ptr: list,
+            cur: list,
 
-			_marker: PhantomData,
-		}
-	}
+            _marker: PhantomData,
+        }
+    }
 }
 
 impl<'a> Iterator for Devices<'a> {
-	type Item = Device<'a>;
+    type Item = Device<'a>;
 
-	fn next(&mut self) -> Option<Self::Item> {
-		if self.cur.is_null() {
-			return None;
-		}
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur.is_null() {
+            return None;
+        }
 
-		unsafe {
-			let info = Device::new(self.cur);
-			self.cur = (*self.cur).next;
+        unsafe {
+            let info = Device::new(self.cur);
+            self.cur = (*self.cur).next;
 
-			Some(info)
-		}
-	}
+            Some(info)
+        }
+    }
 }
 
 impl<'a> Drop for Devices<'a> {
-	fn drop(&mut self) {
-		unsafe {
-			hid_free_enumeration(self.ptr);
-		}
-	}
+    fn drop(&mut self) {
+        unsafe {
+            hid_free_enumeration(self.ptr);
+        }
+    }
 }
--- src/manager.rs
+++ src/manager.rs
@@ -1,56 +1,54 @@
-use std::sync::atomic::{AtomicBool, ATOMIC_BOOL_INIT, Ordering};
+use std::sync::atomic::{AtomicBool, Ordering, ATOMIC_BOOL_INIT};
 
-use sys::*;
-use error::{self, Error};
 use devices::Devices;
+use error::{self, Error};
+use sys::*;
 
 static INITIALIZED: AtomicBool = ATOMIC_BOOL_INIT;
 
 /// The device manager.
 pub struct Manager;
 
-unsafe impl Send for Manager { }
+unsafe impl Send for Manager {}
 
 /// Create the manager.
 pub fn init() -> error::Result<Manager> {
-	if INITIALIZED.load(Ordering::Relaxed) {
-		return Err(Error::Initialized);
-	}
+    if INITIALIZED.load(Ordering::Relaxed) {
+        return Err(Error::Initialized);
+    }
 
-	let status = unsafe { hid_init() };
+    let status = unsafe { hid_init() };
 
-	if status != 0 {
-		return Err(Error::from(status));
-	}
+    if status != 0 {
+        return Err(Error::from(status));
+    }
 
-	INITIALIZED.store(true, Ordering::Relaxed);
+    INITIALIZED.store(true, Ordering::Relaxed);
 
-	Ok(Manager)
+    Ok(Manager)
 }
 
 impl Drop for Manager {
-	fn drop(&mut self) {
-		let status = unsafe { hid_exit() };
+    fn drop(&mut self) {
+        let status = unsafe { hid_exit() };
 
-		if status != 0 {
-			panic!("hid_exit() failed");
-		}
+        if status != 0 {
+            panic!("hid_exit() failed");
+        }
 
-		INITIALIZED.store(false, Ordering::Relaxed);
-	}
+        INITIALIZED.store(false, Ordering::Relaxed);
+    }
 }
 
 impl Manager {
-	/// Find the wanted device, `vendor` or `product` are given it will
-	/// returns only the matches devices.
-	pub fn find(&self, vendor: Option<u16>, product: Option<u16>) -> Devices {
-		unsafe {
-			Devices::new(vendor, product)
-		}
-	}
+    /// Find the wanted device, `vendor` or `product` are given it will
+    /// returns only the matches devices.
+    pub fn find(&self, vendor: Option<u16>, product: Option<u16>) -> Devices {
+        unsafe { Devices::new(vendor, product) }
+    }
 
-	/// Return all devices.
-	pub fn devices(&self) -> Devices {
-		self.find(None, None)
-	}
+    /// Return all devices.
+    pub fn devices(&self) -> Devices {
+        self.find(None, None)
+    }
 }
